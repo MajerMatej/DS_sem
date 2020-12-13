725c0f7a7ccdc23ca7879e72b6b883c984c46fa0
Matej Majer <mmajer351@gmail.com>
2020-12-13 17:32:18 +0100

    Connection to remote server completed

--- MusicStore/src/musicstore/MusicStore.java
+++ MusicStore/src/musicstore/MusicStore.java
@@ -14,28 +14,19 @@ public class MusicStore {
      */
     public static void main(String[] args) {
         try{  
-            //step1 load the driver class  
+            
             Class.forName("oracle.jdbc.driver.OracleDriver");  
-
-            //step2 create  the connection object  
-            Connection con=DriverManager.getConnection(  
-            "jdbc:oracle:thin:@localhost:1521:xe","matej_majer","akosamas");  
-
-            //step3 create the statement object  
+            
+            Connection con=DriverManager.getConnection("jdbc:oracle:thin:@asterix.fri.uniza.sk:1521:orcl", "kojda3", "***");   
             Statement stmt=con.createStatement();  
 
-            //step4 execute query  
-            ResultSet rs=stmt.executeQuery("select * from os_udaje");  
+            ResultSet rs=stmt.executeQuery("select * from store_table");  
             while(rs.next())  
-            System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3) 
-                    + " " + rs.getString(4)+"  "+rs.getString(5)+"  "+rs.getString(6));  
+            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3) 
+                    + " " + rs.getString(4));//+"  "+rs.getString(5)+"  "+rs.getString(6));  
 
-            //step5 close the connection object  
             con.close();  
 
-            }catch(Exception e){ System.out.println(e);}  
-  
-        
+            }catch(Exception e){ System.out.println(e);}        
     }
-    
 }
